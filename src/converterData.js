784d56b5b2f8eaf0abf862b2d69c2905004f377d
Joao Aurelio Francisco Junior <jafrancisco@RDL752507.raiadrogasil.com>
2020-11-13 12:31:24 -0300

    joao

--- src/converterData.js
+++ src/converterData.js
@@ -1,9 +1,21 @@
-import { format } from 'date-fns';
+import { format, parse } from 'date-fns';
 
 export default (data) => {
 
-    let string = JSON.stringify(data);
-    let novaData = new Date (string.replace("-", "/"));
-    return format (novaData, "dd/MM/yyyy");
+    let string = JSON.stringify(data+" 0:0:0"); 
+    
+    string=string.replaceAll(':',',');
+    string=string.replaceAll(' ',',');
+    string=string.replaceAll('-',',');
+    string=string.replaceAll('"','');
+    
+    string=string.split(',');
+
+
+    let novaData = new Date (parseInt(string[0]), parseInt(string[1]), 
+    parseInt(string[2]),parseInt(string[3]), parseInt(string[4]), parseInt(string[5]));
+        
+    
+    return format(novaData, "dd/MM/yyyy");
 
 }
